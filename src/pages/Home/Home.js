3b9713b3c20ba53d6e2b76b3481e14204224165f
myvagrantlife <edward@myvagrantlife.com>
2020-12-09 16:54:57 +0100

    update homepage styling

--- src/pages/Home/Home.js
+++ src/pages/Home/Home.js
@@ -1,10 +1,21 @@
 import React from "react"
-import { Hero } from "components"
+
+import { Hero, CardList } from "components"
+import { Container, Title } from "./Home.style"
 
 const Home = () => (
   <div>
     <Hero />
-    <h1>Home page</h1>
+    <Container>
+      <Title>Latest Bastards</Title>
+      <CardList
+        items={[
+          { id: 1, title: "Edward" },
+          { id: 2, title: "Boy" },
+          { id: 3, title: "Stijn" },
+        ]}
+      />
+    </Container>
   </div>
 )
 
---
+++ src/pages/Home/Home.style.js
@@ -0,0 +1,11 @@
+import styled from "styled-components"
+
+export const Container = styled.div`
+  padding: 2rem 0;
+`
+
+export const Title = styled.h1`
+  font-size: 2.5rem;
+  text-transform: uppercase;
+  margin-bottom: 2rem;
+`
