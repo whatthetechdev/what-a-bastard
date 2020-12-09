bd22fd839270f62c7c0c6384b5968f09cb5de377
myvagrantlife <edward@myvagrantlife.com>
2020-12-09 12:44:25 +0100

    update project folder structure

---
+++ src/app/App.js
@@ -0,0 +1,15 @@
+import React from "react"
+
+// import logo from "../assets/logo.svg"
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <h1>What a Bastard!</h1>
+      </header>
+    </div>
+  )
+}
+
+export default App
