905165cf9a2a7dfe2f160d791cc334e6cf402186
myvagrantlife <edward@myvagrantlife.com>
2020-12-10 20:42:18 +0100

    show a bastard on the about page

--- src/pages/About/About.style.js
+++ src/pages/About/About.style.js
@@ -1,8 +1,10 @@
 import styled from "styled-components"
 
 export const Container = styled.div`
-  padding: 2rem 0;
+  padding: 4rem 8rem;
   line-height: 180%;
+  display: flex;
+  justify-content: space-between;
 
   p {
     margin-bottom: 20px;
@@ -12,4 +14,23 @@ export const Container = styled.div`
 export const Title = styled.h1`
   font-size: 2rem;
   margin-bottom: 2rem;
+`
+
+export const LeftColumn = styled.div`
+  margin-right: 5%;
+  width: 55%;
+`
+export const RightColumn = styled.div`
+  flex: 1;
+`
+
+export const BastardName = styled.h2`
+  font-size: 1.5em;
+  margin-bottom: 1rem;
+`
+
+export const BastardImage = styled.img`
+  border: 20px solid #000000;
+  width: 420px;
+  height: 300px;
 `
